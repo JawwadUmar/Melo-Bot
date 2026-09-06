77ffda8445b513a6bdb314d86f937b0bb91458a2
Jawwad <jawwadumar99@gmail.com>
2026-09-07 01:09:28 +0530

    added exluded jobs

---
+++ app/config/excluded_jobs.py
@@ -0,0 +1,51 @@
+excluded_job_title_keywords = [
+    "Wordpress",
+    "Internship",
+    "Intern",
+    "Web Developer",
+    "Mobile App Developer",
+    "Security Engineer",
+    "Guidewire Digital Developer",
+    "Guidewire",
+    "Test Automation Engineer",
+    "Shopify Developer",
+    "Web Engineer",
+    "ServiceNow Developer",
+    "Mainframe Developer",
+    "Frontend",
+    "Front-end",
+    "Salesforce Developer",
+    "MDM Developer",
+    ".Net",
+    "Unity Developer",
+    "Junior",
+    "ORMB Developer",
+    "Shopify",
+    "Expert Windows Driver Engineer",
+    "PHP"
+]
+
+excluded_company_keywords = [
+    "Tata Consultancy Services",
+    "People Prime Worldwide",
+    "Lala Company"
+]
+
+def isExcludedJob(job_title: str, company_name: str) -> bool:
+    for company in excluded_company_keywords:
+        if company.lower() in company_name.lower():
+            print(
+                f"🤖 Zozo: Skipping job because company is in the exclusion list: "
+                f"{company_name}"
+            )
+            return True
+
+    for keyword in excluded_job_title_keywords:
+        if keyword.lower() in job_title.lower():
+            print(
+                f"🤖 Zozo: Skipping job because job title contains an excluded keyword: "
+                f"{job_title}"
+            )
+            return True
+
+    return False
